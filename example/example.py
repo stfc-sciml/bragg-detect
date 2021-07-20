4569e1b7c5e3551fd03c59dec2d87c713165afa5
Kuangdai Leng <kuangdai.leng@earth.ox.ac.uk>
2021-07-20 21:44:49 +0100

    Update example.py

--- example/example.py
+++ example/example.py
@@ -1,29 +1,28 @@
-import h5py
 import numpy as np
 
 from bragg_detect import detect_bragg_peaks, plot_peaks_over_data
 
 if __name__ == '__main__':
     # read data
-    print(f'Data file: data.hdf5')
-    with h5py.File(f'data.hdf5', 'r') as file:
-        data = file[list(file.keys())[0]][:]
+    with h5py.File('data.hdf5', 'r') as h5:
+        dset = list(h5.keys())[0]
+        data = h5[dset][:]
 
     # detect
     peaks = detect_bragg_peaks(data, large_peak_size=[10, 10, 50],
                                threshold=.2, workers=1)
 
     # save peak locations
-    np.savetxt(f'result/peak_locations.txt', peaks, fmt='%d')
+    np.savetxt('result/peak_locations.txt', peaks, fmt='%d')
 
     # plot image without peaks
     plot_peaks_over_data(data, plot_size=(2000, 1000, 3000),
                          vmax=(.5, 0.05, 0.05),
                          peak_sets=[], axis_on=True,
-                         save_to_file=f'result/input_image.png')
+                         save_to_file='result/input_image.png')
 
     # plot image with peaks
     plot_peaks_over_data(data, plot_size=(2000, 1000, 3000),
                          vmax=(.5, 0.05, 0.05),
                          peak_sets=[(peaks, 'w', 'o', 3, 0)], axis_on=True,
-                         save_to_file=f'result/peaks_on_image.png')
+                         save_to_file='result/peaks_on_image.png')
